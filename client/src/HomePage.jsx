c4c3f3b33671e7c91e0f5922ef9d8a9fc55bb3d5
Brandon205 <brandonblack02@gmail.com>
2019-11-08 08:16:52 -0800

    Made jsx components and finished backend routes

---
+++ client/src/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends Component {
+  state = { 
+
+  }
+
+  render() { 
+    return ( 
+      <h1>This is the HomePage component</h1>
+    );
+  }
+}
+
+export default HomePage;
---
+++ client/src/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Profile extends Component {
+  state = { 
+
+  }
+
+  render() { 
+    return ( 
+      <h1>This is the Profile component</h1>
+    );
+  }
+}
+
+export default Profile;
