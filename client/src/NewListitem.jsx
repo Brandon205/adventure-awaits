c9729cfefd74505c5666f17addd963f9bf0ae449
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-11-08 15:05:47 -0800

    working get and post route

--- client/src/NewListitem.jsx
+++ client/src/NewListitem.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
 import axios from 'axios';
 
-class NewListitem extends Component {
+class NewListitem extends React.Component {
   state = { 
     name: '',
     description: '',
     photo: '',
-    categories = []
+    categories: []
   }
   componentDidMount = () => {
     axios.get('/api/categories')
     .then(response => {
       this.setState({
-        categories = response.data
+        categories: response.data
       })
     })
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ 
+      [e.target.name]: e.target.value 
+    });
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
+    console.log('working')
     var categoriesCopy = [...this.state.categories]
     axios.post('/api/categories', {
       name: this.state.name,
+      description: this.state.description,
+      photo: this.state.photo,
     }).then( response => {
-      categoriesCopy = response.data
+      categoriesCopy.push(response.data)
       this.setState({
         categories: categoriesCopy,
+        description: '',
+        photo: '',
         name: '',
       })
     })
   }
   render() { 
     
-    const mappedCategories = this.state.categories.map( (category,id) => <option value={category._id}>{category.name}</option>)
-
+    const mappedCategories = this.state.categories.map( (category,id) => <option key={id} value={category._id}>{category.name}</option>)
     return ( 
       <>
       <h1>This is the NewListitem component</h1>
-      <form>
-      <input type="text" onChange={this.handleChange} name="name" value="" placeholder="Add to your bucketlist"/>
+      <form onSubmit={this.handleSubmit}>
+      <input type="text" onChange={this.handleChange} name="name" value={this.state.name} placeholder="Add to your bucketlist"/>
         <input type="hidden" onChange={this.handleChange} name="description" value=""/>
         <input type="hidden" onChange={this.handleChange} name="photo" value=""/> 
         
