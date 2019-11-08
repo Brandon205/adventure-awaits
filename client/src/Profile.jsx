7b3e82c4b59bf227e52cd2f92a0eb70028ee2ae4
Ari <gryphon150@gmail.com>
2019-11-08 12:30:26 -0800

    fixed error in Profile.jsx

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class Profile extends React.Component {
 
   render() { 
     return ( 
+      <>
       <Link to="/profile/new"> Create your new Adventure! </Link> <br /> 
+      </>
     );
   }
 }
