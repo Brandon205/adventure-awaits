0412d6c0754d68b3bcaf9cfe66886446158dc1f2
Brandon205 <brandonblack02@gmail.com>
2019-11-12 16:17:18 -0800

    Added the ability for a user to check things off

--- client/src/Bucketlist.jsx
+++ client/src/Bucketlist.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 class Bucketlist extends React.Component {
   state = { 
-    category: [],
+    category: null,
     listItems: []
   }
 
@@ -19,10 +19,25 @@ class Bucketlist extends React.Component {
     })
   }
 
+  handleChecked = (e, id) => {
+    e.preventDefault();
+    console.log(e.target);
+    let config = {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`
+      }
+    }
+    Axios.put(`/api/profile/${e.target.name}`, {checked: e.target.checked}, config).then(res => {
+      let listItemCopy = [...this.state.listItems]
+      listItemCopy[id] = res.data
+      this.setState({ listItems: listItemCopy })
+    })
+  }
+
   render() { 
     var mappedListitems;
     if (this.state.listItems.length > 0 && this.state.category.length > 0) {
-      mappedListitems = this.state.listItems.map((listItem, id) => <div key={id}><li><Link to={`/profile/${listItem._id}/adventure/${this.props.match.params.cName}`}>{listItem.name}</Link> - <Link to={`/profile/${listItem._id}/edit/${this.props.match.params.cName}`}>Edit</Link> </li> </div>)
+      mappedListitems = this.state.listItems.map((listItem, id) => <div key={id}><input type="checkbox" checked={listItem.checked} name={listItem._id} onChange={(e) => this.handleChecked(e, id)} /><Link to={`/profile/${listItem._id}/adventure/${this.props.match.params.cName}`}>{listItem.name}</Link> - <Link to={`/profile/${listItem._id}/edit/${this.props.match.params.cName}`}>Edit</Link> </div>)
     } else {
       mappedListitems = <li>Loading...</li>
     }
